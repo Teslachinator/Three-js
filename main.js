151f6b58fc528dfe43e45f8e2c22b19a72037ccb
Teslachinator <kantov2001@mail.ru>
2023-03-15 23:58:13 +0300

    Updates

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // import * as THREE from "../node_modules/three/build/three.module.js";
-import { THREE } from 'three'
+import * as THREE from 'three'
 
 const canvas = document.querySelector("#c");
 //
